04ce42d4c835e7beb303551c56fb65e6059d1d93
Jacey Davis <jaceyd923@gmail.com>
2023-02-08 12:39:30 -0700

    wrote simple calculator functions

--- calculator/calculator.test.js
+++ calculator/calculator.test.js
@@ -1,37 +1,40 @@
-//require function
+const sum = require('./calculator');
+const subtract = require('./calculator');
+const multiply = require('./calculator');
+const divide = require('./calculator');
 
 //SUM
 test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+    expect(sum.sum(1, 2)).toBe(3);
 });
 
 test('adds 4 + 5 to equal 9', () => {
-    expect(sum(4, 5)).toBe(9);
+    expect(sum.sum(4, 5)).toBe(9);
 });
 
 //SUBTRACT
 test('subtract 3 - 2 to equal 1', () => {
-    expect(subtract(3, 2)).toBe(1);
+    expect(subtract.subtract(3, 2)).toBe(1);
 });
 
-test('subtract 3 - 2 to equal 1', () => {
-    expect(subtract(10.6, 6)).toBe(4.6);
+test('subtract 10.6 - 6 to equal 1', () => {
+    expect(subtract.subtract(10.6, 6)).toBe(4.6);
 });
 
 //MULTIPLY
 test('multiplies 5 * 2 to equal 10', () => {
-    expect(multiply(5, 2)).toBe(10);
+    expect(multiply.multiply(5, 2)).toBe(10);
 })
 
 test('multiplies 6 * 6 to equal 36', () => {
-    expect(multiply(6, 6)).toBe(36);
+    expect(multiply.multiply(6, 6)).toBe(36);
 })
 
 //DIVIDE
 test('divides 10 / 2 to equal 5', () => {
-    expect(divide(10, 2)).toBe(5);
+    expect(divide.divide(10, 2)).toBe(5);
 })
 
 test('divides 16 / 8 to equal 2', () => {
-    expect(divide(16, 8)).toBe(2);
+    expect(divide.divide(16, 8)).toBe(2);
 })
